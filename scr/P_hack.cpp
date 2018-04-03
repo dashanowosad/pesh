b0836c42565ecea7495c158e0cbe81d45933d9a0
Dasha <dashanowosad@yandex.ru>
2018-04-03 15:27:49 +0700

    Add source files in folder scr

--- scr/P_hack.cpp
+++ scr/P_hack.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "functions.h"
+
 using namespace std;
 void P_hack(char k[5], char A[9][9], int &z4, int &flag1)
 {
--- scr/p_forward.cpp
+++ scr/p_forward.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "functions.h"
+
 using namespace std;
 
 void p_forward(char k[5], char A[9][9], int &z1, int &flag1)
